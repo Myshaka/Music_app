ecdeb9507e06ca132e180623ef47d2290aa19a68
W57467 <w57467@student.wsiz.rzeszow.pl>
2018-05-12 13:29:22 +0200

    trending

---
+++ app/src/main/java/com/example/stud/musicapp/api/trendingList.java
@@ -0,0 +1,14 @@
+package com.example.stud.musicapp.api;
+
+/**
+ * Created by W57467 on 2018-05-12.
+ */
+import java.util.List;
+
+public class trendingList {
+
+    public List<trendingSingle> trending ;
+
+
+}
+
---
+++ app/src/main/java/com/example/stud/musicapp/api/trendingSingle.java
@@ -0,0 +1,19 @@
+package com.example.stud.musicapp.api;
+
+/**
+ * Created by W57467 on 2018-05-12.
+ */
+
+public class trendingSingle {
+        public int idAlbum ;
+        public int idArtist ;
+        public int idTrack ;
+        public int idTrend ;
+        public int intChartPlace ;
+        public String strAlbum ;
+        public String strArtist ;
+        public String strTrack ;
+        public String strTrackThumb ;
+    }
+
+
