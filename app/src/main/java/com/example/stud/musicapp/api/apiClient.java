ead850b7aa87bc47beea61cfdbc0ab411f786703
W57467 <w57467@student.wsiz.rzeszow.pl>
2018-05-26 13:02:08 +0200

    pobieranie danych

--- app/src/main/java/com/example/stud/musicapp/api/apiClient.java
+++ app/src/main/java/com/example/stud/musicapp/api/apiClient.java
@@ -1,11 +1,17 @@
 package com.example.stud.musicapp.api;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 /**
  * Created by W57467 on 2018-05-12.
  */
 
 public interface apiClient {
 
-
+    @GET( "trending.php" )
+    Call<trendingList> getTrendingList(@Query( "country" ) String country, @Query ( "type" )
+            String type, @Query ( "format" ) String format);
 
 }
