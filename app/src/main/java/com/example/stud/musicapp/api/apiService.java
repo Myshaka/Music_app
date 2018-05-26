56d03cc345576cee9b40a55fa81155264dcd3edc
W57467 <w57467@student.wsiz.rzeszow.pl>
2018-05-26 12:29:27 +0200

    gson

--- app/src/main/java/com/example/stud/musicapp/api/apiService.java
+++ app/src/main/java/com/example/stud/musicapp/api/apiService.java
@@ -1,6 +1,7 @@
 package com.example.stud.musicapp.api;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by W57467 on 2018-05-12.
@@ -11,6 +12,7 @@ public class apiService {
     public static apiClient getService(){
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl( "http://www.theaudiodb.com/api/v1/json/{1}/" )
+                .addConverterFactory(GsonConverterFactory.create ())
                 .build();
 
         return retrofit.create(apiClient.class);
