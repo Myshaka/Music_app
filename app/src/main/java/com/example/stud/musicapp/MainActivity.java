fc7e3e47f1bd9ebf4bb6666d50ba2900750a3397
W57467 <w57467@student.wsiz.rzeszow.pl>
2018-06-23 11:36:31 +0200

    zad_dom

--- app/src/main/java/com/example/stud/musicapp/MainActivity.java
+++ app/src/main/java/com/example/stud/musicapp/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.stud.musicapp;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.example.stud.musicapp.topsongs.TopSongsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +17,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button bTopSongs = findViewById(R.id.bTopSongs);
+
         bTopSongs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "TODO", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "TODO", Toast.LENGTH_SHORT).show();
+                //uruchamianie layoutu
+                Intent intent = new Intent(MainActivity.this, TopSongsActivity.class);
+                startActivity(intent);
             }
         });
 
+
     }
-} //komentarz
+}
